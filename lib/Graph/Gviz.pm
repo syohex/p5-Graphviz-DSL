226fa7ec1e957daddd43d805dfd6c600129e6a8a
Syohei YOSHIDA <syohex@gmail.com>
2012-10-06 19:18:13 +0900

    init repos

---
+++ lib/Graph/Gviz.pm
@@ -0,0 +1,42 @@
+package Graph::Gviz;
+use strict;
+use warnings;
+use 5.008_001;
+
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Graph::Gviz -
+
+=head1 SYNOPSIS
+
+  use Graph::Gviz;
+
+=head1 DESCRIPTION
+
+Graph::Gviz is
+
+=head1 AUTHOR
+
+Syohei YOSHIDA E<lt>syohex@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2012- Syohei YOSHIDA
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
